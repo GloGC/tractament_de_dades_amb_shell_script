3ac327b68557883e7efd80a1ff54f1718d66701e
GloGC <1710155@uab.cat>
2023-10-19 15:04:12 +0200

    actualització 1

--- tractament_de_dades_amb_shell_script.sh
+++ tractament_de_dades_amb_shell_script.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+CADENA='(("\w+( \w+)")|\w+)'
+opcion='x'
+codi_p='xx'
+codi_e='xxx'
+echo "Introduce una opción:"
+while [ "$opcion" != 'q' ]; do
+	read opcion
+	case $opcion in
+		q)
+			echo "Saliendo de la aplicación"
+			exit
+			;;
+ 
