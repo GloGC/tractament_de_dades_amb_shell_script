0fda931dee45fcf2fd2c30d858234c54bc149245
GloGC <1710155@uab.cat>
2023-10-19 15:23:00 +0200

    actualització 3

--- tractament_de_dades_amb_shell_script.sh
+++ tractament_de_dades_amb_shell_script.sh
@@ -14,5 +14,19 @@ while [ "$opcion" != 'q' ]; do
 		lp)
 			cut -d',' -f7,8 cities.csv | column -s',' -t | uniq | head -3
 			;;
+		sc)
+			echo "Introduce el nombre de un país:"
+			read pais
+			if grep -q "$pais" cities.csv; then
+				cut -d',' -f7,8 cities.csv > pais.csv
+				codi_p="$(grep "$pais" pais.csv | cut -d',' -f1 | uniq)"
+				echo "$codi_p"
+			elif [ -z "$pais" ]; then
+				echo "Opción no válida"
+			else
+				codi_p='XX'
+				echo "$codi_p"
+			fi
+			;;
 	
  
